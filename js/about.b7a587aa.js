e890549aa7799097866c6b9db4eca8c7b573fc3d
rakshit koyani <rakshitkoyani456@gmail.com>
2020-05-11 15:26:23 +0530

    first commit

---
+++ js/about.b7a587aa.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var o=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About")]),n("p",[t._v("Welcome to Todo list application. You can add and remove your task according to your preferance.")])])}],c=n("2877"),r={},u=Object(c["a"])(r,o,a,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=about.b7a587aa.js.map
